5c17749409065dfc6ca1b8092f87ea98e53dd222
Op-Pautu <pautuop@gmail.com>
2023-08-19 22:43:21 +0530

    Add some style for home page and icons

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -32,20 +32,7 @@ export default async function Home() {
         )}
 
       </section>
-      {/* <section className="mt-9 flex flex-col gap-10">
-        {result.threads.length === 0 ? (
-          <p className="no-result">No threads found</p>
-        ): (
-          <>
-            {result.threads.map(thread) => (
-              <ThreadCard 
-                key={thread._id}
-                id={}
-                />
-            )}
-          </>
-        )}
-      </section> */}
+    
       {/* <UserButton afterSignOutUrl="/"/> */}
     </>
   )
