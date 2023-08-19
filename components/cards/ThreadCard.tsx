c1332293d8bf916820ff40397fa694daee5cafb9
Op-Pautu <pautuop@gmail.com>
2023-08-20 00:02:50 +0530

    Add reply functionality

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -37,7 +37,7 @@ const ThreadCard = ({
     isComment
 }: Props) => {
     return (
-        <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+        <article className={`flex w-full flex-col rounded-xl ${isComment ? "px-0 xs:px-7": "bg-dark-2 p-7"}`}>
             <div className="flex items-start justify-between">
                 <div className="flex w-full flex-1 flex-row gap-4">
                     <div className="flex flex-col items-center">
@@ -67,11 +67,11 @@ const ThreadCard = ({
                                 <Image src="/assets/share.svg" alt="icon" width={24} height={24} className="cursor-pointer object-contain"/>
                             </div>
 
-                            {isComment && comments.length > 0 && (
+                            {/* {!isComment && comments.length > 0 && (
                                 <Link href={`/thread/${id}`}>
                                     <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
                                 </Link>
-                            )}
+                            )} */}
                         </div>
                     </div>
                 </div>
